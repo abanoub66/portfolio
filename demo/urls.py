f738d8fd71914098f93c3b5781206d9efdc88628
Abanoub <faragaba@gmail.com>
2022-01-19 15:11:07 -0800

    pls fix

--- demo/urls.py
+++ demo/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.number_list_create, name='number_list_create'),
     path('number_list_create', views.number_list_create, name='number_list_create'),
     path('sorting_list/<current>', views.sorting_list, name='sorting_list'),
+    path('sorting_list/<current>/sort/<s>', views.sort, name='sort'),
     path('previous_demos', views.previous_demos, name='previous_demos'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, redirect
-from .models import Demo
-from . import forms
+import os.path
+import subprocess
 from os import listdir
 from os.path import isfile, join
+from subprocess import STDOUT, PIPE
+
+from django.shortcuts import render, redirect
+
+from . import forms
+from .models import Demo
+
+java_file_path = 'R:\\farag\\PyCharmProjects\\portfolio\\demo\\JavaSortingMethods'
 
 
 def number_list_create(request):
@@ -18,9 +25,18 @@ def number_list_create(request):
 
 
 def sorting_list(request, current):
-    my_path = 'R:\\farag\\PyCharmProjects\\portfolio\\demo\\JavaSortingMethods'
-    sorts = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+    sorts = [f for f in listdir(java_file_path) if isfile(join(java_file_path, f))]
     return render(request, "demo/sorting_list.html", {'sorts': sorts, 'current': current})
+
+
+def sort(request, current, s):
+    subprocess.check_call(['javac', java_file_path + '\\' + s])
+    java_class, ext = os.path.splitext(java_file_path + '\\' + s)
+    cmd = ['java', java_class]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout, stderr = proc.communicate()
+    print('This was "' + stdout.decode('UTF-8') + '"')
+    return render(request, "demo/sort_anim.html", {'current': current, s: 's'})
 
 
 def previous_demos(request):
